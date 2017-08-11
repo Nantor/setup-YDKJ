2bfb200415d8276314ba4d496c468bf138329f0a
spammail001@gmx.de <?6n\PU=m#x4Z&ihwu31.S93\Ft+U@;dY>
2017-08-11 22:48:52 +0200

    implement resetting the highscore

--- YDKJ-setup.py
+++ YDKJ-setup.py
@@ -2,6 +2,7 @@
 import argparse
 import importlib
 import os
+import pathlib
 import pkgutil
 
 import setup
@@ -14,6 +15,7 @@ class Arguments:
         self.intro = None
         self.dir = None
         self.locale = None
+        self.reset_highscore = False
 
 
 if __name__ == '__main__':
@@ -22,6 +24,12 @@ if __name__ == '__main__':
 
     parser_all = argparse.ArgumentParser(description='Setup your "You don\'t know Jack" game.',
                                          conflict_handler='resolve')
+    parser_all.add_argument(
+        '--resetHighscore',
+        dest='reset_highscore',
+        action='store_true',
+        help='resetting the highscore list'
+    )
     subparsers = parser_all.add_subparsers(help='local help')
     locales = {}
 
@@ -29,7 +37,7 @@ if __name__ == '__main__':
         sub_parser = subparsers.add_parser(name, dest='locale', help=name + ' help', conflict_handler='resolve')
         locales[name] = importlib.import_module('.' + name, _package)
         locales[name].extend_parser(sub_parser)
-    parser_all.add_argument('dir', nargs=1, help='directory of the YDKJ game')
+    parser_all.add_argument('dir', nargs=1, dist='dir', help='directory of the YDKJ game')
 
     parser_all.parse_args(namespace=args)
 
@@ -43,3 +51,8 @@ if __name__ == '__main__':
 
     if not os.path.isfile(highscore) or not os.path.isfile(USD):
         raise dir_error
+
+    if args.reset_highscore:
+        highscore_path = pathlib.Path(highscore)
+        highscore_path.unlink()
+        highscore_path.touch()
